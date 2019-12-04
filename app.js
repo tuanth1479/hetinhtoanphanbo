7a184e4796deee9b279f8269d91f775b7f342539
Dinh Dinh <dinhtd@gmail.com>
2019-12-04 09:48:54 -0800

    push for fun

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
+
 app.get('/dev', function (req, res) {
   res.send('hello dev');
 });
 
+
 app.listen(process.env.PORT || 3000);
  
 module.exports = app;
+
+
+
+
+
+
+
